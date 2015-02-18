a5fd328aa50103e6e91081bbc363658622490e95
asotelo137 <asotelo.pghs@gmail.com>
2015-02-17 18:00:34 -0800

    Update isr.c
    Added CRP checks , still need queues.

--- cpe159/phase1/potato/isr.c
+++ cpe159/phase1/potato/isr.c
@@ -16,22 +16,36 @@ void CreateISR(int pid) {
 }
 
 void TerminateISR() {
-   just return if CRP is 0 or -1 (Idle or not given)
-      
-   change state of CRP to NONE
-   queue it to none queue
-   set CRP to -1 (none)
+   //just return if CRP is 0 or -1 (Idle or not given)
+    if (pcb_t.state<=0){
+      return 0;
+    }  
+   //change state of CRP to NONE
+   //queue it to none queue
+   //set CRP to -1 (none)
+   
+   pcb_t.state= NONE;
+   CRP=-1;
 }        
 
 void TimerISR() {
-   just return if CRP is Idle (0) or less (-1)
-
-   upcount the runtime of CRP
-
-   if the runtime of CRP reaches TIME_LIMIT
+  // just return if CRP is Idle (0) or less (-1)
+   if (pcb_t.state <= 0 ){
+      return 0;
+   }
+   
+   //upcount the runtime of CRP
+   pcb_t.runtime++;
+   
+   /*if the runtime of CRP reaches TIME_LIMIT
    (need to rotate to next PID in run queue)
       sum up runtime to the total runtime of CRP
       change its state to RUN
       queue it to run queue
       reset CRP (to -1, means none)
+   */
+   if(pcb_t.runtime == TIME_LIMIT){
+      pcb_t.total_runtime=pcb_t.runtime + pcb_t.total_runtime;
+      
+   }
 }
