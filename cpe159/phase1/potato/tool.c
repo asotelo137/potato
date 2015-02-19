ea48d2a2e5b8bb1f977610d6edc6d504db61d728
asotelo137 <asotelo.pghs@gmail.com>
2015-02-18 21:07:12 -0800

    Update tool.c

--- cpe159/phase1/potato/tool.c
+++ cpe159/phase1/potato/tool.c
@@ -5,39 +5,16 @@
 #include "type.h"
 #include "extern.h"
 
-//typedef struct node *node_ptr;
-//typedef struct queue *queue_ptr;
-
-/*struct node{
-	int data;
-	struct node *prev;
-	struct node *next;
-}*/
-
-/*struct queue{
-	node_ptr front;
-	node_ptr back;
-}*/
 
 void MyBZero(q_t *p, int size) {
-/*	queue_ptr myqueue;
-	init(&myqueue);
-	node_ptr new = (node_ptr)malloc(sizeof(struct node));
-	myqueue->front = new;
-*/
+
 	int i;
 	for (i = 1; i < 20; i++){
 	EnQ(0,p);
 	}
 }
 
-/*int init(queue_ptr * qp){
-	queue_ptr q = (queue_ptr)malloc(sizeof(struct queue));
-	q->front = NULL;
-	q->back = NULL;
-	*qp = q;	//set qp
-}
-*/
+
 void EnQ(int pid, q_t *p) {
 // ?????????????????????????????????????????????????
 // show error msg and return if queue's already full
