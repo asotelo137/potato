4c9353e0b30939e0b505421fd55029e436e489b3
asotelo137 <asotelo.pghs@gmail.com>
2015-02-19 00:22:02 -0800

    Update main.c

--- cpe159/phase1/potato/main.c
+++ cpe159/phase1/potato/main.c
@@ -35,8 +35,8 @@ void InitData() {
    queue PID's 1~19 (skip 0) into none_q (un-used PID's)
    set state to NONE in all un-used pcb[1~19]
    set CRP to 0 (Idle proc ID)*/
-   MyBZero(&run_q,Q_SIZE);
-   MyBZero(&none_q,Q_SIZE);
+   MyBZero(&run_q,0);
+   MyBZero(&none_q,0);
    i = 1;
    for(i  ; i<20;i++){
       pcb[i].state = NONE;
