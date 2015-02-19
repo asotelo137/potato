e27c9145590e93faa12e16b11726b0ab71a3e565
asotelo137 <asotelo.pghs@gmail.com>
2015-02-18 23:33:42 -0800

    Update main.c

--- cpe159/phase1/potato/main.c
+++ cpe159/phase1/potato/main.c
@@ -65,6 +65,7 @@ void SelectCRP() {       // select which PID to be new CRP
    //if no processes to run (check size in run queue against zero)
    //   set CRP to 0 (at least we can run Idle proc)
    if(run_q.size == 0 ){
+      printf("run_q size = 0 ");
       CRP = 0;
    }else{      
       //set CRP to first in run queue (dequeue it)
