d8a25dad1dc93191f9d4d9824127baa2f1ac5c09
asotelo137 <asotelo.pghs@gmail.com>
2015-02-20 00:00:36 -0800

    Update proc.c

--- cpe159/phase1/usetrapframe/proc.c
+++ cpe159/phase1/usetrapframe/proc.c
@@ -5,23 +5,19 @@
 #include "extern.h"  // for current_run_pid needed here below
 #include "proc.h"    // for Idle, SimpleProc, DispatchProc
 
-void Dispatch() {
-   if(CRP == 0) {//if CRP is 0:
-    Idle();  //call Idle() as proc run
-   }else //(all other processes)
-      //call UserProc() as proc run
-      UserProc();
-   
-}
 
 void Idle() {
    int i;
+   for(;;){
    cons_printf("0");//print 0 on PC
    for(i=0; i<1666000; i++) IO_DELAY();//busy-loop delay for about 1 sec
+   } 
 }
 
 void UserProc() {
    int i;
+    for(;;){
    cons_printf("%d",CRP);//print its pid (CRP) on PC
    for(i=0; i<1666000; i++) IO_DELAY();//busy-loop delay for about 1 sec
+    }
 }
