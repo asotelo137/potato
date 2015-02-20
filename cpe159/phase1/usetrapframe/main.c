ccb746969832732cde0e8a4ed08232ab362a30c2
asotelo137 <asotelo.pghs@gmail.com>
2015-02-19 23:56:34 -0800

    Update main.c

--- cpe159/phase1/usetrapframe/main.c
+++ cpe159/phase1/usetrapframe/main.c
@@ -3,27 +3,42 @@
 //
 // Team Name: ??????? (Members: ?????? and ??????)
 
+//include statements...
 #include "spede.h"      // spede stuff
 #include "main.h"       // main stuff
 #include "isr.h"        // ISR's
 #include "tool.h"       // handy functions
 #include "proc.h"       // processes such as Init()
 #include "type.h"       // processes such as Init()
+#include "entry.h"
 
 // kernel data structure:
 int CRP;                // current running PID, -1 means no process
 q_t run_q, none_q;      // processes ready to run and not used
 pcb_t pcb[MAX_PROC];    // process table
 char stack[MAX_PROC][STACK_SIZE]; // run-time stacks for processes
+//(include stuff from timer lab and new PCB described in 1.html)
+struct i386_gate *IDT_ptr;
+
+void SetEntry(int entry_num, func_ptr_t func_ptr){
+   struct i386_gate *gateptr = &IDT_ptr[entry_num];
+   fill_gate(gateptr,(int)func_ptr,get_cs(),ACC_INTR_GATE,0);
+}
+
+void InitIDT(){
+   IDT_ptr = get_idt_base();//locate IDT
+   cons_printf("IDT is at %u. \n",IDT_ptr);
+   SetEntry(32,TimerEntry);//prime IDT Entry
+   outportb(0x21,~1);
+}
 
 int main() {
    InitData(); 		//call Init Data to initialize kernel data
    
    CreateISR(0);	//call CreateISR(0) to create Idle process (PID 0)
-
-   while (1) {      // alter 2 things below
-      Dispatch();    // to dispatch/run CRP
-      Kernel();      // for kernel control
+   Dispatch(pcb[0].TF_ptr);    // to dispatch/run CRP
+   
+   return 0;
    }
 
    //return 0;
@@ -31,15 +46,12 @@ int main() {
 
 void InitData() {
    int i;
-   /*initialize 2 queues (use MyBzero() you code in tool.c/.h)
-   queue PID's 1~19 (skip 0) into none_q (un-used PID's)
-   set state to NONE in all un-used pcb[1~19]
-   set CRP to 0 (Idle proc ID)*/
+   
    MyBZero(&run_q,0);
    MyBZero(&none_q,0);
    
-   i = 1;
-   for(i  ; i<Q_SIZE;i++){
+   
+   for(i = 1 ; i<Q_SIZE;i++){
       pcb[i].state = NONE;
       EnQ(i,&none_q);
       
@@ -48,11 +60,6 @@ void InitData() {
 }
 
 void SelectCRP() {       // select which PID to be new CRP
-   
-//    printf("Selcet CRP  Beggineing CRP %d \n",CRP);
-   /*simply return if CRP is greater than 0 (already good one selected)
-   (continue only when CRP is Idle or none (0 or -1)
-   */ 
    if(CRP > 0){
       return;
    }    
@@ -60,33 +67,33 @@ void SelectCRP() {       // select which PID to be new CRP
    if(CRP == 0){
       pcb[0].state = RUN;
    }
-//    printf("after checking if its zero or greater CRP %d \n",CRP);
-   //if no processes to run (check size in run queue against zero)
-//   printf("run_q size = %d \n",run_q.size);
-   //   set CRP to 0 (at least we can run Idle proc)
+
    if(run_q.size == 0 ){
-//      printf("run_q size = %d \n ",run_q.size);
       CRP = 0;
    }else{  
-//       printf("after checking run q size CRP %d \n",CRP);
-      //set CRP to first in run queue (dequeue it)
       CRP = DeQ(&run_q); 
-//      printf("after DeQ in selectcrp %d \n",CRP);
-      //change mode in PCB of CRP to UMODE
       pcb[CRP].mode = UMODE;
-      //change state in PCB of CRP to RUNNING
       pcb[CRP].state = RUNNING;
    }
 }
 
-void Kernel() {
+void Kernel(TF_t *TF_ptr) {
    int pid,i;
    char key;
-
+   pcb[CRP].TF_ptr=TF_ptr;
    //change state in PCB of CRP to kernel mode
-   pcb[CRP].state = KMODE;
+   pcb[CRP].mode = KMODE;
    //call TimerISR() to service timer interrupt as it just occurred
-   TimerISR();
+   switch(TF_ptr->intr_num){
+      case TIMER_INTR:
+         TimerISR();
+         break;
+      default:
+         cons_printf("Panic!\n");
+         breakpoint();
+         break;
+   }
+   
    if (cons_kbhit()) {
       key = cons_getchar(); // key = cons_getchar();
       switch(key) {
@@ -120,7 +127,7 @@ void Kernel() {
       }                                                              // end switch
    }                                                                 // end if some key pressed
 //   printf("after case statement \n");
-   SelectCRP();                                                       //call SelectCRP() to settle/determine for next CRP
-   
+   SelectCRP();    //call SelectCRP() to settle/determine for next CRP
+   Dispatch(pcb[CRP].TF_ptr);
 }
 
