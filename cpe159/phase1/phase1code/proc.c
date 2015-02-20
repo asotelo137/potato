bbf6234242a067aba13b6b59368237e28f67a57f
asotelo137 <asotelo.pghs@gmail.com>
2015-02-19 20:18:40 -0800

    Update proc.c

--- cpe159/phase1/phase1code/proc.c
+++ cpe159/phase1/phase1code/proc.c
@@ -7,15 +7,21 @@
 
 
 void Idle() {
+   int tick;
+   while(1){
    cons_printf("0");//print 0 on PC
-   for(;;){ 
-      IO_DELAY();//busy-loop delay for about 1 sec
+      for(tick= 0 ;tick <16660000;tick++){ 
+         IO_DELAY();//busy-loop delay for about 1 sec
+      }
    }
 }
 
 void UserProc() {
-   cons_printf("%d",CRP);//print its pid (CRP) on PC
-   for(;;){ 
-      IO_DELAY();//busy-loop delay for about 1 sec
+   int tick;
+   while(1){
+      cons_printf("%d",CRP);//print its pid (CRP) on PC
+      for(tick =0 ;tick < 16660000;tick++){ 
+         IO_DELAY();//busy-loop delay for about 1 sec
+      }
    }
 }
