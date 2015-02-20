2f899b9861365e6f17ef1153f4cebb61f57b3ee5
asotelo137 <asotelo.pghs@gmail.com>
2015-02-20 00:18:37 -0800

    Update main.c

--- cpe159/phase1/usetrapframe/main.c
+++ cpe159/phase1/usetrapframe/main.c
@@ -37,6 +37,7 @@ int main() {
    CreateISR(0);	//call CreateISR(0) to create Idle process (PID 0)
    InitIDT();
    CRP=0;
+   EI();
    Dispatch(pcb[0].TF_ptr);    // to dispatch/run CRP
    
    return 0;
