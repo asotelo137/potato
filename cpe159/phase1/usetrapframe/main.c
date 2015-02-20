900b69a0514cd7421369cec8ce75a75081f4f5b1
asotelo137 <asotelo.pghs@gmail.com>
2015-02-20 00:09:11 -0800

    Update main.c

--- cpe159/phase1/usetrapframe/main.c
+++ cpe159/phase1/usetrapframe/main.c
@@ -36,7 +36,7 @@ int main() {
    InitData(); 		//call Init Data to initialize kernel data
    
    CreateISR(0);	//call CreateISR(0) to create Idle process (PID 0)
-   Dispatch(pcb[0].TF_ptr);    // to dispatch/run CRP
+   //Dispatch(pcb[0].TF_ptr);    // to dispatch/run CRP
    
    return 0;
    
