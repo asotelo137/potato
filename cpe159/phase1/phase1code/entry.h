1b5e5589f7f6666fbae95de81d4545159c81d2e2
asotelo137 <asotelo.pghs@gmail.com>
2015-02-19 21:52:36 -0800

    Update entry.h

--- cpe159/phase1/phase1code/entry.h
+++ cpe159/phase1/phase1code/entry.h
@@ -8,29 +8,29 @@
 .text
 // Loader() loads registers from trap frame
 ENTRY(Loader)
-movl 4(%esp), %eax // load stack pointer from eax
-movl %eax, %esp
-popl %gs
-popl %fs
-popl %es
-popl %ds
-popa // restore general registers
-add $4, %esp // skip 4 bytes that stored the intr_id
-iret
+  movl 4(%esp), %eax // load stack pointer from eax
+  movl %eax, %esp
+  popl %gs
+  popl %fs
+  popl %es
+  popl %ds
+  popa // restore general registers
+  add $4, %esp // skip 4 bytes that stored the intr_id
+  iret
 // push intr type number then jump to common handler
 ENTRY(TimerEntry)
-pushl $TIMER_INTR
-jmp EnterKernel
-// Common IRQ entry, save context and call Kernel()
-EnterKernel:
-pusha
-pushl %ds
-pushl %es
-pushl %fs
-pushl %gs
-movl %esp, %ecx
-cld
-SET_KERNAL_SEGS // set the DS and ES to kernel's data segments
-leal kernal_stack + KERNAL_STACK_SIZE, %esp
-pushl %ecx
-call CNAME(Kernel)
+  pushl $TIMER_INTR
+  jmp EnterKernel
+  // Common IRQ entry, save context and call Kernel()
+  EnterKernel:
+  pusha
+  pushl %ds
+  pushl %es
+  pushl %fs
+  pushl %gs
+  movl %esp, %ecx
+  cld
+  SET_KERNAL_SEGS // set the DS and ES to kernel's data segments
+  leal kernal_stack + KERNAL_STACK_SIZE, %esp
+  pushl %ecx
+  call CNAME(Kernel)
