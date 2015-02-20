2434d557d764d073f7ada6a77c656ae58822a204
asotelo137 <asotelo.pghs@gmail.com>
2015-02-19 21:56:42 -0800

    Update entry.h

--- cpe159/phase1/phase1code/entry.h
+++ cpe159/phase1/phase1code/entry.h
@@ -1,36 +1,17 @@
-// entry.S, 159
-// entry points of interrupts
-#include <spede/machine/asmacros.h>
-#include "entry.h"
-// set DS and ES to Kernel's data segment
-#define SET_KERNAL_SEGS movw $(SEL_KDATA), %ax; mov %ax, %ds; mov %ax, %es
-.comm kernal_stack, KERNAL_STACK_SIZE, 1 // define kernel stack space
-.text
-// Loader() loads registers from trap frame
-ENTRY(Loader)
-  movl 4(%esp), %eax // load stack pointer from eax
-  movl %eax, %esp
-  popl %gs
-  popl %fs
-  popl %es
-  popl %ds
-  popa // restore general registers
-  add $4, %esp // skip 4 bytes that stored the intr_id
-  iret
-// push intr type number then jump to common handler
-ENTRY(TimerEntry)
-  pushl $TIMER_INTR
-  jmp EnterKernel
-  // Common IRQ entry, save context and call Kernel()
-  EnterKernel:
-  pusha
-  pushl %ds
-  pushl %es
-  pushl %fs
-  pushl %gs
-  movl %esp, %ecx
-  cld
-  SET_KERNAL_SEGS // set the DS and ES to kernel's data segments
-  leal kernal_stack + KERNAL_STACK_SIZE, %esp
-  pushl %ecx
-  call CNAME(Kernel)
+// entry.h, 159
+#ifndef _ENTRY_H_
+#define _ENTRY_H_
+#include <spede/machine/pic.h>
+#define TIMER_INTR 32
+#define KCODE 0x08 // kernel's code segment
+#define KDATA 0x10 // kernel's data segment
+#define KSTACK_SIZE 8192 // kernel's stack size, in chars
+// ISR Entries
+#ifndef ASSEMBLER
+__BEGIN_DECLS
+#include "type.h" // for trapframe_t below
+extern void TimeEntry(); // code defined in entry.S
+extern void Dispatch(TF_t *); // code defined in entry.S
+__END_DECLS
+#endif
+#endif
