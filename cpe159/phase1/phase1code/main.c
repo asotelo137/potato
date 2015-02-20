b2600d540d37d36f546aef2edef8e395a5d8eb31
asotelo137 <asotelo.pghs@gmail.com>
2015-02-19 21:31:39 -0800

    Update main.c

--- cpe159/phase1/phase1code/main.c
+++ cpe159/phase1/phase1code/main.c
@@ -10,8 +10,8 @@
 #include "proc.h"       // processes such as Init()
 #include "type.h"       // processes such as Init()
 #include "entry.h"
-//#include "entry.S"
-// Speede Includes
+
+
 #include <spede/stdio.h>
 #include <spede/flames.h>
 #include <spede/machine/io.h>
@@ -27,7 +27,7 @@ char stack[MAX_PROC][STACK_SIZE]; // run-time stacks for processes
 //(include stuff from timer lab and new PCB described in 1.html)
 
 
-//typedef void (* func_ptr_t)();
+
 struct i386_gate *IDT_ptr;
 void InitIDT();
 //SetEntry() needed from timer lab
@@ -38,10 +38,6 @@ void SetEntry(int entry_num, func_ptr_t func_ptr){
 //InitData() still the same as PureSimulation
 void InitData() {
    int i;
-   /*initialize 2 queues (use MyBzero() you code in tool.c/.h)
-   queue PID's 1~19 (skip 0) into none_q (un-used PID's)
-   set state to NONE in all un-used pcb[1~19]
-   set CRP to 0 (Idle proc ID)*/
    MyBzero(&run_q,0);
    MyBzero(&none_q,0);
    
@@ -54,11 +50,6 @@ void InitData() {
 
 //SelectCRP() still the same as PureSimulation
 void SelectCRP() {       // select which PID to be new CRP
-   
-   //    printf("Selcet CRP  Beggineing CRP %d \n",CRP);
-   /*simply return if CRP is greater than 0 (already good one selected)
-   (continue only when CRP is Idle or none (0 or -1)
-   */ 
    if(CRP > 0){
       return;
    }    
@@ -66,10 +57,7 @@ void SelectCRP() {       // select which PID to be new CRP
    if(CRP == 0){
       pcb[0].state = RUN;
    }
-   //    printf("after checking if its zero or greater CRP %d \n",CRP);
-   //if no processes to run (check size in run queue against zero)
-   //   printf("run_q size = %d \n",run_q.size);
-   //   set CRP to 0 (at least we can run Idle proc)
+   
    if(run_q.size == 0 ){
    //      printf("run_q size = %d \n ",run_q.size);
       CRP = 0;
@@ -109,28 +97,6 @@ int main() {
    return 0;
 }
 
-
-/*
-void Kernel(TF_t *TF_ptr) {
-   change to kernel mode for CRP
-   save TF_ptr to PCB of CRP
-
-   switch upon intr_num pointed by TF_ptr to call corresponging ISR {
-      if it's TIMER_INTR:
-         call TimerISR()
-      default:
-         PANIC! msg and break into GDB
-   }
-
-// still handles other keyed-in simulated events
-   (same as PureSimulation to handle key events)
-
-   call SelectCRP() to select process to run
-   call Dispatch(pcb[CRP].TF_ptr) to load it and run
-}
-*/
-
-
 void Kernel(TF_t *TF_ptr) {
    int pid,i;
    char key;
@@ -156,35 +122,33 @@ void Kernel(TF_t *TF_ptr) {
       key = cons_getchar(); // key = cons_getchar();
       switch(key) {
          case 'n':                                                   //if 'n'
-             // printf("n pressed\n");
             if (none_q.size == 0){                                   //no processes left in none queue
                cons_printf("No more process!\n");                    //"No more process!\n" (msg on target PC)
             }else{
             pid = DeQ(&none_q);                                      //get 1st PID un-used (dequeue none queue)
-            // printf("after pressing n pid is %d \n",pid);
             CreateISR(pid);                                          //call CreateISR() with it to create new process
             i = 1;
             for(i  ; i<Q_SIZE;i++){
-            // printf("%d is in runque %d\n",run_q.q[i],i);
+            
                
             }
                
             }
             break;
          case 't':                                                   //if 't'
-            //printf("t pressed\n");
+           
             TerminateISR();                                          //call TerminateISR() to terminate CRP
             break;   
          case 'b':                                                   //if 'b'
-            //printf("b pressed \n");
+           
             breakpoint();                                            // this goes back to GDB prompt
             break;
          case 'q':                                                   //if 'q'
-            // printf("q pressed\n");
+            
             exit(0);                                                 //just do exit(0);
       }                                                              // end switch
    }                                                                 // end if some key pressed
-   //printf("after case statement \n");
+  
    SelectCRP();                                                       //call SelectCRP() to settle/determine for next CRP
    Dispatch(pcb[CRP].TF_ptr);
 }
