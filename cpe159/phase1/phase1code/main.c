318903a3eff5825fecfbd765fe4820723f1c0e3b
asotelo137 <asotelo.pghs@gmail.com>
2015-02-19 20:27:47 -0800

    Update main.c

--- cpe159/phase1/phase1code/main.c
+++ cpe159/phase1/phase1code/main.c
@@ -10,7 +10,7 @@
 #include "proc.h"       // processes such as Init()
 #include "type.h"       // processes such as Init()
 #include "entry.h"
-#include "entry.S"
+//#include "entry.S"
 // Speede Includes
 #include <spede/stdio.h>
 #include <spede/flames.h>
