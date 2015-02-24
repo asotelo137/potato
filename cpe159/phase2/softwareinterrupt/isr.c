cb5df1d76b2172f31de5397b317b91b534404983
asotelo137 <asotelo.pghs@gmail.com>
2015-02-24 01:01:00 -0800

    Update isr.c

--- cpe159/phase2/softwareinterrupt/isr.c
+++ cpe159/phase2/softwareinterrupt/isr.c
@@ -62,7 +62,7 @@ void TerminateISR() {
 
 void GetPidISR(){
   
- // outportb(0x20,0x60);
+ outportb(0x20,0x60);
   pcb[CRP].TF_ptr->ebx = CRP;
   GetPid();
   
@@ -104,7 +104,7 @@ void TimerISR() {
 
 
 void SleepISR(int seconds){
-  //outportb(0x20,0x60);
+  outportb(0x20,0x60);
   wake_period= sys_time+(100*seconds);
   pcb[CRP].wake_time=wake_period;
   EnQ(CRP,&sleep_q);
