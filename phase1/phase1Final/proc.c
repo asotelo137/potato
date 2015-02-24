de6ff7724d873770b19f073a71c1c9de71e63320
Aaron Sotelo <soteloa@athena.ecs.csus.edu>
2015-02-23 22:33:50 -0800

    my initial Message

---
+++ phase1/phase1Final/proc.c
@@ -0,0 +1,23 @@
+// proc.c, 159
+// processes are here
+
+#include "spede.h"   // for IO_DELAY() needed here below
+#include "extern.h"  // for current_run_pid needed here below
+#include "proc.h"    // for Idle, SimpleProc, DispatchProc
+
+
+void Idle() {
+   int i;
+   for(;;){
+   cons_printf("0");//print 0 on PC
+   for(i=0; i<1666000; i++) IO_DELAY();//busy-loop delay for about 1 sec
+   } 
+}
+
+void UserProc() {
+   int i;
+    for(;;){
+   cons_printf("%d",CRP);//print its pid (CRP) on PC
+   for(i=0; i<1666000; i++) IO_DELAY();//busy-loop delay for about 1 sec
+    }
+}
