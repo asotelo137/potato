0f6362ab260f76138de40f3f1aa75d3e1310b29e
asotelo137 <asotelo.pghs@gmail.com>
2015-03-02 18:04:42 -0800

    Update main.c

--- phase3/semaphores/main.c
+++ phase3/semaphores/main.c
@@ -44,10 +44,10 @@ void InitData() {
    int i;
    sys_time = 0;
    
-   MyBZero(&run_q,0);
-   MyBZero(&none_q,0);
-   MyBZero(&sleep_q,0);
-   MyBZero(&semaphore_q,0);
+   MyBZero((char *)&run_q,0);
+   MyBZero((char *)&none_q,0);
+   MyBZero((char *)&sleep_q,0);
+   MyBZero((char *)&semaphore_q,0);
    
    for(i = 1 ; i<Q_SIZE;i++){
       pcb[i].state = NONE;
