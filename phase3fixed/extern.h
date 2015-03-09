f037a03be62b4bc1d201081f959ed92076a419ee
asotelo137 <asotelo.pghs@gmail.com>
2015-03-09 02:56:14 -0700

    Update extern.h

--- phase3fixed/extern.h
+++ phase3fixed/extern.h
@@ -10,4 +10,19 @@ extern q_t run_q, none_q,sleep_q;    // ready to run, not used proc IDs
 extern pcb_t pcb[MAX_PROC];  // process table
 extern char stack[MAX_PROC][STACK_SIZE]; // proc run-time stacks
 
+//phase 3
+/*main.c/extern.h
+   apply the semaphore type to declare an array of semaphores,
+   "semaphore," which has the same size as a PID queue
+
+   a semaphore queue "semaphore_q" to be primed with available
+   semaphores (numbers 1 ~ 19),
+    
+   need for testing (Phase 3 only, after Phase, delete):
+      integers "product_semaphore" and "product"
+*/
+extern semaphore_t semaphore;
+extern q_t semaphore_q;
+extern int product_semaphore,product;
+
 #endif
