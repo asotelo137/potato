de1992ebde00713fe1d84eed635ce4174e0d6aa8
asotelo137 <asotelo.pghs@gmail.com>
2015-03-09 18:07:22 -0700

    Update extern.h

--- phase3/semaphores/extern.h
+++ phase3/semaphores/extern.h
@@ -9,6 +9,6 @@ extern int CRP,sys_time,product,product_semaphore;              // PID of curren
 extern q_t run_q, none_q,sleep_q,semaphore_q;    // ready to run, not used proc IDs
 extern pcb_t pcb[MAX_PROC];  // process table
 extern char stack[MAX_PROC][STACK_SIZE]; // proc run-time stacks
-extern semaphore_t semaphore[MAX_PROC];
+extern semaphore_t semaphore[MAX_PROC];//
 
 #endif
