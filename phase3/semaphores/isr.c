a81f952152369ba79002154c5dc8583ba471ff3c
asotelo137 <asotelo.pghs@gmail.com>
2015-03-09 18:22:50 -0700

    Update isr.c

--- phase3/semaphores/isr.c
+++ phase3/semaphores/isr.c
@@ -120,7 +120,7 @@ void SleepISR(int seconds){
 
 void SemWaitISR(){
   
-  int semID = GetPid();
+  int *semID = GetPid();
   printf("wait ISR");
   if(semaphore[semID].count > 0){
     semaphore[semID].count --;
@@ -137,7 +137,7 @@ void SemPostISR(){
 
 
 
- int semID = GetPid();
+ int *semID = GetPid();
    printf("post ISR");
    breakpoint();
   if(semaphore[semID].wait_q.size ==0){
