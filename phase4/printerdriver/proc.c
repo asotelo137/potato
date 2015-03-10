79d9997b5c23e44fc1e7f06d733d23eaaf047368
asotelo137 <asotelo.pghs@gmail.com>
2015-03-10 13:33:23 -0700

    Update proc.c

--- phase4/printerdriver/proc.c
+++ phase4/printerdriver/proc.c
@@ -62,3 +62,54 @@ void Consumer() {
    }
 }
  
+//Phase 4 ******************************************************************8
+
+void Print_Driver(){
+   int TIME_OUT=3*1666000;
+   int i, code;
+   char str []= "Hello, my team is called PotatoOS!\n Its time to bake potato!\n\0";
+   char *p;
+   
+   print_semaphore = SemGet(0); // should it be -1? depends on IRQISR();
+   
+   // make sure printer's powered up, cable connected, the following
+   // statements (until the Sleep call) will reset the printer and the
+   // first IRQ 7 will occur
+   outportb(LPT1_BASE+LPT_CONTROL, PC_SLCTIN); // Printer Control, SeLeCT INterrupt
+   code = inportb(LPT1_BASE+LPT_STATUS);
+   for(i=0; i<50; i++) IO_DELAY();             // needs delay
+   outportb(LPT1_BASE+LPT_CONTROL, PC_INIT|PC_SLCTIN|PC_IRQEN); // IRQ ENable
+   Sleep(1);                                   // printer needs time to reset
+
+   while(1) {
+      cons_printf("PID is %d.",pid);// my PID
+      Sleep(1);  //Sleep for 1 second
+      if(print_it == 1){// print_it is 1 (set by Kernel() when key polled is 'p') {
+         p = str;//p copy from str
+         while (p != 0 ){  //what p points to is not 0 {
+            
+            // code sending the character to the port (see above)
+
+            // code busy-poll for printer readiness (see above)
+            // or, do a semaphore wait (for interrupt-driven mode)
+
+            increment pointer p (to point to the next character)
+         } // while what p...
+      } // if print_it...
+      set print_it back to 0
+   } // while(1)
+} // PrintDriver()
+   
+}
+
+//phase 4 *********************************************************
+void IRQ7(){
+   int pid;
+   
+   outportb(20,67);
+   if(semaphore[print_semaphore].wait_q.size>0){
+      pid = DeQ(sem[print].wait);
+      EnQ(pid,&run_q);
+      pcb[pid].state=RUN;
+   }
+}
