9ba83e8a04545f09306608b6e4925aee6e47358c
asotelo137 <asotelo.pghs@gmail.com>
2015-03-09 17:10:57 -0700

    Update isr.c

--- phase3fixed/isr.c
+++ phase3fixed/isr.c
@@ -28,10 +28,18 @@ void CreateISR(int pid) {
       pcb[pid].TF_ptr = (TF_t *)&stack[pid][STACK_SIZE];
       pcb[pid].TF_ptr--;
       // fill out trapframe of this new proc:
-      if(pid == 0)
+      /*if(pid == 0)
       pcb[pid].TF_ptr->eip = (unsigned int)Idle; // Idle process
       else
       pcb[pid].TF_ptr->eip = (unsigned int)UserProc; // other new process
+      */
+      if(pid == 0)
+        pcb[pid].TF_ptr->eip = (unsigned int)Idle; // Idle process
+      else if(pid%2 == 0)
+        pcb[pid].TF_ptr->eip = (unsigned int)Consumer;
+      else if(pid%2 == 1)
+        pcb[pid].TF_ptr->eip = (unsigned int)Producer;
+        
       pcb[pid].TF_ptr->eflags = EF_DEFAULT_VALUE | EF_INTR;
       pcb[pid].TF_ptr->cs = get_cs();
       pcb[pid].TF_ptr->ds = get_ds();
