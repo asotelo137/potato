66e42a2ccf6422a28190f831a919f9c46d745a54
asotelo137 <asotelo.pghs@gmail.com>
2015-03-10 15:25:50 -0700

    Update syscall.c

--- phase4/printerdriver/syscall.c
+++ phase4/printerdriver/syscall.c
@@ -44,8 +44,8 @@ int SemGet(int count){
    int semaphoreID;
    asm(" movl %%ebx,%1;int $52;movl %%ecx, %0" // CPU inst
       : "=g" (semaphoreID) // 1 output from asm()
-      : "g"(count)// 1 input into asm()
-      : ); // push/pop before/after asm()
+      : "g"(count);)// 1 input into asm()
+      //:  // push/pop before/after asm()
    return semaphoreID;
    
 }
