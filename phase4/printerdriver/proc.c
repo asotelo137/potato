34d0036c19eacd188da746f20d55eeab73e9a028
asotelo137 <asotelo.pghs@gmail.com>
2015-03-10 20:13:46 -0700

    Update proc.c

--- phase4/printerdriver/proc.c
+++ phase4/printerdriver/proc.c
@@ -65,21 +65,16 @@ void Consumer() {
  
 //Phase 4 ******************************************************************8
 
-void PrintDriver(){
-   // send ch to data reg and control code to control reg
-   // then repeatedly poll for printer ACK unless timed out
-   // #include <spede/machine/parallel.h> // flag constants used below
-   //int TIME_OUT=3*1666000; // time out 3 secs
+void PrintDriver() {
    int i, code;
-   char str []= "Hello, my team is called PotatoOS!\n It's time to bake potato!\n\0";
+   char str[] = "Hello, my Team is called (team name here)!\n\0";
    char *p;
-   
-   print_semaphore = SemGet(0); // should it be -1? depends on IRQISR();
-   //TIME_OUT_COUNT =0;
-   
-   // make sure printer's powered up, cable connected, the following
-   // statements (until the Sleep call) will reset the printer and the
-   // first IRQ 7 will occur
+
+   print_semaphore = SemGet(0); // should it be -1? depends on IRQISR()
+
+// make sure printer's powered up, cable connected, the following
+// statements (until the Sleep call) will reset the printer and the
+// first IRQ 7 will occur
    outportb(LPT1_BASE+LPT_CONTROL, PC_SLCTIN); // Printer Control, SeLeCT INterrupt
    code = inportb(LPT1_BASE+LPT_STATUS);
    for(i=0; i<50; i++) IO_DELAY();             // needs delay
@@ -87,52 +82,25 @@ void PrintDriver(){
    Sleep(1);                                   // printer needs time to reset
 
    while(1) {
-      
-      cons_printf("PID is %d.\n",GetPid());// my PID
-      Sleep(1);  //Sleep for 1 second
-      if(print_it == 1){// print_it is 1 (set by Kernel() when key polled is 'p') {
-         p = str;//p copy from str
-         while (*p ){  //what p points to is not 0 {
+      cons_printf("My pid is : %d", GetPid());
+      Sleep(1);
+      if (print_it == 1 )//(set by Kernel() when key polled is 'p') {
+         p =str;
+         while (*p){//what p points to is not 0 {
+
             // code sending the character to the port (see above)
-                     
             outportb(LPT1_BASE+LPT_DATA, *p);      // send char to data reg
             code = inportb(LPT1_BASE+LPT_CONTROL); // read control reg
             outportb(LPT1_BASE+LPT_CONTROL, code|PC_STROBE); // send with added strobe
             for(i=0; i<20; i++) IO_DELAY();        // delay for EPSON LP-571 printer
             outportb(LPT1_BASE+LPT_CONTROL, code); // send original control code
-            
-            
             // code busy-poll for printer readiness (see above)
-            // ----------------- BUSY-POLL PSEUDO CODE BELOW ------------------
-            // if interrupt-driven mode below is replaced by a semaphore wait
-            // loop 3*1666000 (3 seconds) {
-            /*
-            while(TIME_OUT_COUNT != TIME_OUT){
-               //    code = PS_ACK & inportb(LPT1_BASE+LPT_STATUS); // read status
-               code = PS_ACK & inportb(LPT1_BASE+LPT_STATUS); // read status
-               //    if(code == 0) break;    // printer ACK'ed
-               if(code == 0) break;    // printer ACK'ed
-               //    IO_DELAY();             // or, wait 0.65us, try again
-               IO_DELAY();             // or, wait 0.65us, try again
-               // }
-               TIME_COUNT_OUT++;   
-            }
-            // if the loop count did get to that many (timed out) 
-            if ( TIME_OUT_COUNT == TIME_OUT ){
-               //    show msg: printer timed out. Check printer!
-               cons_printf("printer timed out. Check printer!\n");
-               // }
-            }
             // or, do a semaphore wait (for interrupt-driven mode)
-            */
-            //increment pointer p (to point to the next character)
             SemWait(print_semaphore);
-            p++;
+            p++;//increment pointer p (to point to the next character)
          } // while what p...
-         
       } // if print_it...
-       print_it = 0;//set print_it back to 0
+      print_it = 0;
    } // while(1)
-   
 } // PrintDriver()
    
