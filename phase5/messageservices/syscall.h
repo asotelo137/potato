65ac9e450e83b38bc2db4d56e68e16cfde222c97
Joeybrennan17 <Joeybrennan14@gmail.com>
2015-04-04 22:30:19 -0700

    Update syscall.h

--- phase5/messageservices/syscall.h
+++ phase5/messageservices/syscall.h
@@ -2,6 +2,10 @@
 
 #ifndef _SYSCALL_H_
 #define _SYSCALL_H_
+///Phase 5///
+#define MSGSND 53
+#define MSGRCV 54
+/////////////
 
 int GetPid();      // no input, 1 return
 void Sleep(int);   // 1 input, no return
@@ -9,5 +13,8 @@ void SemWait(int);
 void SemPost(int);
 // phase 4 printing
 int SemGet(int);
+//phase 5
+void MsgSnd(msg_t *);
+void MsgRcv(msg_t *);
 
 #endif
