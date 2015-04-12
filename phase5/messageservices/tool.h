f6915b49ffae3ffb6f9e67d323b90e2cf7c93578
asotelo137 <asotelo.pghs@gmail.com>
2015-04-12 00:39:13 -0700

    Update tool.h

--- phase5/messageservices/tool.h
+++ phase5/messageservices/tool.h
@@ -10,7 +10,7 @@ int DeQ(q_t *);
 void EnQ(int, q_t *);
 void MsgEnQ(msg_t *p, msg_q_t *q);
 msg_t *MsgDeQ(msg_q_t *p);
-void MyStrcpy(char *dest, char *src);
+void MyStrCpy(char *dest, char *src);
 
 #endif
 
