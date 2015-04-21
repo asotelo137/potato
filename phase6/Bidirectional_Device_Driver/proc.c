d9de4282f274973ef223cb4d5dca401741314ef2
asotelo137 <asotelo.pghs@gmail.com>
2015-04-21 11:31:47 -0700

    Update proc.c

--- phase6/Bidirectional_Device_Driver/proc.c
+++ phase6/Bidirectional_Device_Driver/proc.c
@@ -287,11 +287,11 @@ void STDOUT(){
 
       while(1){//loop A (until p points to null):
          SemWait(terminal.TX_sem);//semaphore-wait on TX_sem of terminal interface
-         EnQ((int) p,terminal.TX_q)//enqueue what p points to to TX_q of terminal interface
+         EnQ((int) p,&terminal.TX_q)//enqueue what p points to to TX_q of terminal interface
          TipIRQ3();//issue syscall "TipIRQ3();" to manually start IRQ 3
          if(*p == '\n'){//if what p points to is '\n' {
             SemWait(terminal.TX_sem);//semaphore-wait on TX_sem of terminal interface
-            EnQ((int) '\r',terminal.TX_q)//enqueue '\r' to TX_q  of terminal interface
+            EnQ((int) '\r',&terminal.TX_q)//enqueue '\r' to TX_q  of terminal interface
          }
          p++;//advance p
       }//repeat loop A
