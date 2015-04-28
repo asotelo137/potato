47e7be7038deecf3ee95aeebb7b1f77f1ba10314
asotelo137 <asotelo.pghs@gmail.com>
2015-04-27 21:27:55 -0700

    Update proc.c

--- phase7/File_Manger/proc.c
+++ phase7/File_Manger/proc.c
@@ -335,6 +335,7 @@ void ShellDirStr(attr_t *p, char *str) {
    }
 void ShellDir(char *cmd, int STDOUT, int FileMgr) {
       //phase 7 
+      char *p;
    int FILEMGR = 6;
       char obj[101], str[101];
       msg_t msg;
@@ -371,7 +372,7 @@ void ShellDir(char *cmd, int STDOUT, int FileMgr) {
    // otherwise, code is good, returned msg has an "attr_t" type,
    // check if user directed us to a file, then "dir" for that file;
    // write code:
-   char * p = (attr_t *)msg.data;
+   p = (attr_t *)msg.data;
    
    if( ! A_ISDIR(p->mode) ) {
       ShellDirStr(p, str);        // str will be built and returned
