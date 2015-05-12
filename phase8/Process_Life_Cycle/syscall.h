e494964e610e7e80346dbcfe90ac4b99989554bb
asotelo137 <asotelo.pghs@gmail.com>
2015-05-11 18:04:41 -0700

    Update syscall.h

--- phase8/Process_Life_Cycle/syscall.h
+++ phase8/Process_Life_Cycle/syscall.h
@@ -19,5 +19,9 @@ void MsgSnd(msg_t *);
 void MsgRcv(msg_t *);
 //phase6
 void TipIRQ3();
+//phase 8 
+void Fork(char *); // argument is addr of executable
+int Wait(int *);   // returns PID of exited child, argument is to get exit #
+void Exit(int);    // argument is exit number to return to parent
 
 #endif
